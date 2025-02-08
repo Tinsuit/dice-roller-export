99e970d285a3be83e496838c95ad0154129b6e9d
Tinsuit <122144066+Tinsuit@users.noreply.github.com>
2025-02-08 11:39:32 -0800

    new features

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738974232|6786146986';
+const CACHE_VERSION = '1739043564|3553983709';
 /** @type {string} */
-const CACHE_PREFIX = 'Dice Roller 5-sw-cache-';
+const CACHE_PREFIX = 'Dice Roller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
