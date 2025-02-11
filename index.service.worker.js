2f10ea3366ffe064ac33159b6a6f83abeaa6fc57
Tinsuit <122144066+Tinsuit@users.noreply.github.com>
2025-02-11 12:24:03 -0800

    changed Head Include and Experimental Virtual Keyboard

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739302452|13088162822';
+const CACHE_VERSION = '1739305411|166633240';
 /** @type {string} */
 const CACHE_PREFIX = 'Dice Roller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
