f627c707a150a4103d325e7d2bad78d3e87ea055
Tinsuit <122144066+Tinsuit@users.noreply.github.com>
2025-02-15 12:01:45 -0800

    trying to detect web ios

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739649136|3859176630';
+const CACHE_VERSION = '1739649688|4410690512';
 /** @type {string} */
 const CACHE_PREFIX = 'Dice Roller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
