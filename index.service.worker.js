4fa57ac22ee57daa691d02e9e3b5589221c91407
Tinsuit <122144066+Tinsuit@users.noreply.github.com>
2025-02-17 10:27:49 -0800

    virtual keyboard changes

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739807890|4060336072';
+const CACHE_VERSION = '1739816860|13030970132';
 /** @type {string} */
 const CACHE_PREFIX = 'Dice Roller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
