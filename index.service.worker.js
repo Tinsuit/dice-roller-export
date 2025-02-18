f196ac2ce9e5d25bd18d6647ad4eb52063e74148
Tinsuit <122144066+Tinsuit@users.noreply.github.com>
2025-02-18 11:05:34 -0800

    removed line input, updated button spacing

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739816860|13030970132';
+const CACHE_VERSION = '1739905517|14063885839';
 /** @type {string} */
 const CACHE_PREFIX = 'Dice Roller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
