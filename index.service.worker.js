b7229efa98d187c77cdfa079e13244b7b052844f
Tinsuit <122144066+Tinsuit@users.noreply.github.com>
2025-04-11 14:20:22 -0700

    updated physics and bugfixes

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743632483|2722861010';
+const CACHE_VERSION = '1744406393|1745519850';
 /** @type {string} */
 const CACHE_PREFIX = 'Dice Roller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
